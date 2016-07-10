46965895f955238eddefc840ac3fc517250b0f8f
Gennaro <gennaro.mari@libero.it>
2016-07-10 15:33:11 +0200

    ddd
    Change-Id: If2cad05f01e1ef2b072529ae6d7388708d2898d1
    Signed-off-by: Gennaro <gennaro.mari@libero.it>

--- proj2/src/com/test/Second.java
+++ proj2/src/com/test/Second.java
@@ -9,7 +9,9 @@ public class Second implements Serializable {
 	 */
 	private static final long serialVersionUID = 2622226458910231195L;
 
-
+	public void metodoNoConflict3() {
+		;;;;;;;;;;
+	} 
 	public static void main(String ... args) {
 		System.out.println("Metodo main");
 	}
