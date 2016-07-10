704aad079a6fda4ffdf054ceb34e8d86fccef61a
Gennaro <gennaro.mari@libero.it>
2016-07-10 12:27:41 +0200

    Second update on dev branch
    Change-Id: I8d8d13c92ca4d4d48dfb7bb10b847e42e28868e3
    Signed-off-by: Gennaro <gennaro.mari@libero.it>

--- proj2/src/com/test/Second.java
+++ proj2/src/com/test/Second.java
@@ -1,5 +1,15 @@
 package com.test;
 
-public class Second {
+import java.io.Serializable;
 
+public class Second implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2622226458910231195L;
+
+	public void metodoNoConflict() {
+		;
+	}
 }
