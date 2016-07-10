56d559f131e3d48252ee748a76c28879338ea7cf
Gennaro <gennaro.mari@libero.it>
2016-07-10 12:41:42 +0200

    Merge al contrario
    Change-Id: Iecd72df8a5cc9fab56e691ec06fcd7a835bd9c67
    Signed-off-by: Gennaro <gennaro.mari@libero.it>

--- proj2/src/com/test/Second.java
+++ proj2/src/com/test/Second.java
@@ -9,6 +9,10 @@ public class Second implements Serializable {
 	 */
 	private static final long serialVersionUID = 2622226458910231195L;
 
+
+	public static void main(String ... args) {
+		System.out.println("Metodo main");
+	}
 	public void metodoNoConflict() {
 		;
 	}
